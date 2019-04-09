0d511c4d792bb0fb749c2d6d0a910b8879bbe7df
Ajay1729 <ajay.e.s@lxisoft.com>
2019-04-09 16:40:56 +0530

    adding user model

--- Quiz_V_1.0/com/redlxians/quiz/controller/LoginController.java
+++ Quiz_V_1.0/com/redlxians/quiz/controller/LoginController.java
@@ -5,31 +5,99 @@ package com.redlxians.quiz.controller;
 import com.redlxians.quiz.view.*;
 import java.util.*;
 import java.io.*; 
+import java.util.regex.Pattern;  
+import java.util.regex.Matcher;
 public class LoginController
 {
 
-	public boolean validation(String userName,String password)throws Exception
+	public boolean validation(String emailid,String password)
 	{
+		try{
+		int count=count();
+		System.out.println(count);
 		FileReader reader=new FileReader("db.properties");  
       
     	Properties p=new Properties();  
-    	p.load(reader);  
-      
-   		System.out.println(p.getProperty("MuserName"));  
-	
-		if(userName.equals(p.getProperty("MuserName"))&&password.equals(p.getProperty("Mpassword")))
+    	p.load(reader);   
+		
+		for(int i=0;i<(count/4);i++){ 
+			if(emailid.equals(p.getProperty("email"+i))){
+				if(password.equals(p.getProperty("password"+i))){
+					QuizView q = new QuizView();
+					q.display();
+					return false;
+				}
+			}
+			else{
+				return true;
+			}
+		}
+		if(emailid.equals(p.getProperty("Manager mailid"))&&password.equals(p.getProperty("Manager password")))
 		{
 			ManagerView  m = new ManagerView();
 			m.managerOptions();
 			return false;
 		}
-		else if(userName.equals(p.getProperty("userName"))&&password.equals(p.getProperty("password"))){
-			QuizView q = new QuizView();
-			q.display();
+		else{
 			return false;
 		}
 
+		}
+		catch(IOException e){
+
+		}
 		return true;
+		
 	}
+	public boolean registerValidation(String email,String pass){
+		Scanner sc=new Scanner(System.in); 
+		try{
+	        
+				int count=count();
+	            Pattern pattern = Pattern.compile("\\w+@\\w+\\.[a-z]{2,3}");    
+	            Matcher matcher = pattern.matcher(email);     
+	            if (matcher.find()==true) 
+	            {   
+
+	            	Properties p=new Properties();  
+					p.setProperty("email"+(count/4),matcher.group());
+					p.setProperty("password"+(count/4),pass);  
+	  				
+					p.store(new FileWriter("db.properties",true),"");    
+	                return true;    
+	            }  
+	            else
+	            {    
+	                System.out.println("Entered email pattern is illegal try again.");
+	                LoginView login=new LoginView();
+	                login.register();
+	                return true;
+	            }
+	        }
+	    catch(IOException e){
+
+	    }
+
+	    return false;
+	   
+	}
+	public int count()
+	{
+		int count=0;
+	try{
+		FileReader reader=new FileReader(new File("db.properties"));  
+         
+  	 BufferedReader bR=new BufferedReader(reader);
+  	 String data;   
 	
+		while(bR.readLine()!= null)
+		{ 
+			count++;
+        }
+		}
+		catch(IOException e){
+
+		}	
+		return count;
+	}
 }
--- Quiz_V_1.0/com/redlxians/quiz/view/LoginView.java
+++ Quiz_V_1.0/com/redlxians/quiz/view/LoginView.java
@@ -3,35 +3,50 @@ package com.redlxians.quiz.view;
 *@author Sanjana P
 */
 import java.util.*;
+
 import com.redlxians.quiz.controller.LoginController;
 public class LoginView
 {
-	String userName;
+	String emailid;
 	String password;
 	static Scanner in=new Scanner(System.in);
 	LoginController loginController = new LoginController();
-	public void display()
+	public void display(){
+		System.out.println("1.Login\n2.Register");
+		int a = in.nextInt();
+		if(a==1){
+			login();
+		}
+		else if(a==2){
+			register();
+		}
+	}
+	public void login()
 	{
 
 		boolean validate = false;
 		int input = 0;
 		do{
-			System.out.println("Enter the username: ");
-			userName=in.next();
+			System.out.println("Enter the email id of user: ");
+			emailid=in.next();
 			System.out.println("Enter the password: ");
 			password=in.next();
-			try{
-				validate = loginController.validation(userName,password);
-			}
-			catch(Exception e){
 
-			}
+			validate = loginController.validation(emailid,password);
+
 			if(validate){
 				System.out.println("Invalid User\n Press 1 to Try Again");
 				input = in.nextInt();
 			}
 		}while(input==1);
 
+	}
+	public void register(){
+		System.out.println("Enter Email id");
+		String email = in.next();
+		System.out.println("Enter Password");
+		String pass = in.next();
+		loginController.registerValidation(email,pass);
 	}
 
 }
